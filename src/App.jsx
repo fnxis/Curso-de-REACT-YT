37212d98454b84ebe311b5a2c906ca6a80398c8c
fnxis <guiclash4759@gmail.com>
2026-02-26 19:31:00 -0300

    ajustes de CSS

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App(){
 
 
   return(
-    <div className='w-screen h-screen bg-slate-500 flex justify-center p-6'>
+    <div className='w-screen min-h-screen bg-slate-500 flex justify-center p-6'>
       <div className='w-[500px] space-y-5 bg-slate-500'>
         <h1 className=" text-3xl text-slate-100 text-center font-bold">Gerenciador de Tarefas</h1>
         <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
